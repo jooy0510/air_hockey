2ad6466078a236d74d302172bad3f940467d08f5
loud <78677354+jooy0510@users.noreply.github.com>
2023-07-10 10:12:58 +0900

    Update score.c

--- score/score.c
+++ score/score.c
@@ -87,211 +87,162 @@
 #define NOTE_CS8 4435
 #define NOTE_D8  4699
 #define NOTE_DS8 4978
-#define REST 0
 
-#define START_BUTTON 30
-#define LINE_LED 32
-#define BUZZER 34
-#define INPUT 0
-#define OUTPUT 1
-
-int s_btn;
-int past = 0, time = 0, now = millis(), start = 2, flag = 1;
-int c_robot = 0, c_human = 0;
-unsigned long past_led = 0, time_led = 0, now_led = micros();
-int FND[12][12] = {{1, 0, 0, 0, 0, 0, 0}, 
-                   {1, 1, 1, 1, 0, 0, 1},
-                   {0, 1, 0, 0, 1, 0, 0},
-                   {0, 1, 1, 0, 0, 0, 0},
-                   {0, 0, 1, 1, 0, 0, 1},
-                   {0, 0, 1, 0, 0, 1, 0},
-                   {0, 0, 0, 0, 0, 1, 0},
-                   {1, 0, 1, 1, 0, 0, 0}
-                  };
+#define BUZZER 11
 
 int start_melody[] = {
-  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
-  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
-  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
-  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
 
-  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
-  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
-  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
-  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST, 4,
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
 
-  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
-  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
-  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
-  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
 
-  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
-  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
-  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
-  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST, 4,
-  
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
 
-  NOTE_E5,2,  NOTE_C5,2,
-  NOTE_D5,2,   NOTE_B4,2,
-  NOTE_C5,2,   NOTE_A4,2,
-  NOTE_GS4,2,  NOTE_B4,4,  REST,8, 
-  NOTE_E5,2,   NOTE_C5,2,
-  NOTE_D5,2,   NOTE_B4,2,
-  NOTE_C5,4,   NOTE_E5,4,  NOTE_A5,2,
-  NOTE_GS5,2,
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+//Underworld melody
+int underworld_melody[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+//Underwolrd tempo
+int underworld_tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
 };
 
-int end_melody[] = {
-  NOTE_B4, 16, NOTE_B5, 16, NOTE_FS5, 16, NOTE_DS5, 16, //1
-  NOTE_B5, 32, NOTE_FS5, -16, NOTE_DS5, 8, NOTE_C5, 16,
-  NOTE_C6, 16, NOTE_G6, 16, NOTE_E6, 16, NOTE_C6, 32, NOTE_G6, -16, NOTE_E6, 8,
+void setup(void)
+{
+  pinMode(3, OUTPUT);//buzzer
+  pinMode(13, OUTPUT);//led indicator when singing a note
 
-  NOTE_B4, 16,  NOTE_B5, 16,  NOTE_FS5, 16,   NOTE_DS5, 16,  NOTE_B5, 32,  //2
-  NOTE_FS5, -16, NOTE_DS5, 8,  NOTE_DS5, 32, NOTE_E5, 32,  NOTE_F5, 32,
-  NOTE_F5, 32,  NOTE_FS5, 32,  NOTE_G5, 32,  NOTE_G5, 32, NOTE_GS5, 32,  NOTE_A5, 16, NOTE_B5, 8
-};
+}
+void loop()
+{
+  //sing the tunes
+  sing(1);
+  sing(1);
+  sing(2);
+}
+int song = 0;
 
-int s_notes=sizeof(start_melody)/sizeof(start_melody[0])/2; 
-int s_wholenote = (60000 * 4) / 144;
-int s_divider = 0, s_noteDuration = 0;
+void sing(int s) {
+  song = s;
+  if (song == 2) {
+    Serial.println(" 'Underworld Theme'");
+    int size = sizeof(underworld_melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 1000 / underworld_tempo[thisNote];
 
-int notes = sizeof(end_melody) / sizeof(end_melody[0]) / 2;
-int wholenote = (60000 * 4) / 105;
-int divider = 0, noteDuration = 0;
+      buzz(BUZZER, underworld_melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
 
-/*
-시작
-스타트 버튼 입력 = 1
-SCORE 사람, 로봇 0으로 초기화 
-FND 0-0 출력
-라인 LED 3번 깜빡이기 -> 점등 유지
-3분 카운트
-테트리스 노래 출력
-*/
+      buzz(BUZZER, 0, noteDuration);
 
-/*
-끝
-스타트 버튼 = 0
-SCORE FND 5초 간 출력
-라인 LED 3번 깜빡이기 -> 소등
-팩맨 노래 출력
-*/
+    }
 
-void setup(){
-    pinMode(START_BUTTON, INPUT);
-    pinMode(LINE_LED, OUTPUT);
+  } 
+  else {
+    int size = sizeof(start_melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 1000 / tempo[thisNote];
 
-    pinMode(2, INPUT);
-    for(int i=3; i<=13; i++)
-        pinMode(i, OUTPUT);
-    for(int i=22; i<=28; i++)
-        pinMode(i, OUTPUT);
-
-    digitalWrite(13, 0);
-
-    for(int i=10; i<=12; i++)
-      digitalWrite(i, 1);
-
-    s_btn = digitalRead(START_BUTTON);
+      buzz(BUZZER, start_melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      buzz(BUZZER, 0, noteDuration);
+    }
+  }
 }
 
-void loop(){
-    //시작 스위치를 누르면!!
-    if(s_btn == 1){
-        start = 1;
-        c_human = 0;
-        c_robot = 0;
-    }
+void buzz(int targetPin, long frequency, long length) {
+  digitalWrite(13, HIGH);
+  long delayValue = 1000000 / frequency / 2;
+  long numCycles = frequency * length / 1000;
+  for (long i = 0; i < numCycles; i++) {
+    digitalWrite(targetPin, HIGH);
+    delayMicroseconds(delayValue); 
+    digitalWrite(targetPin, LOW); 
+    delayMicroseconds(delayValue); 
+  }
+  digitalWrite(13, LOW);
 
-    if(start){
-        if(now-past >= 180000){ //3분이 지나면
-            start = 0; //start = 0이고
-            goto end; //끝 지점으로 감
-        }
-        if(c_robot == 7 || c_human == 7){ //7점을 내면
-            start = 0; //start = 0이고
-            goto end; //끝 지점으로 감
-        }
-        //라인 LED 3번 깜빡이기
-        for(int i=0; i<3; i++){
-            digitalWrite(LINE_LED, 1);
-            delay(200);
-            digitalWrite(LINE_LED, 0);
-            delay(200);
-        }
-        digitalWrite(LINE_LED, 1);
-
-        //테트리스 bgm
-        for (int thisNote = 0; thisNote < s_notes * 2; thisNote = thisNote + 2) {
-            s_divider = start_melody[thisNote + 1];
-            if (s_divider > 0) {
-                s_noteDuration = (s_wholenote) / s_divider;
-            } 
-            else if (s_divider < 0) {
-            s_noteDuration = (s_wholenote) / abs(s_divider);
-            s_noteDuration *= 1.5; 
-            }
-            tone(BUZZER, start_melody[thisNote], s_noteDuration*0.9);
-            delay(s_noteDuration);
-            noTone(BUZZER);
-        }
-    }
-
-    //포토 리플렉터 신호 감지 -> 1씩 카운트
-    if(digitalRead(2)==1){
-        if(flag == 1){
-            delay(1000);
-            flag = 0; 
-            c_human++;
-            delay(1500);
-        }
-        if(c_human > 7) c_human = 0;
-    }else flag = 1;
-
-    for(int i=0; i<=6; i++){
-        digitalWrite(i+3, FND[c_robot][i]);
-    }
-
-    for(int i=0; i<=6; i++){
-        digitalWrite(i+22, FND[c_human][i]);
-    }
-
-    //끝 지점
-    end:
-    if(start == 0){
-        //팩맨 노래 출력
-        for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
-            divider = end_melody[thisNote + 1];
-            if (divider > 0) {
-            noteDuration = (wholenote) / divider;
-            } else if (divider < 0) {
-            noteDuration = (wholenote) / abs(divider);
-            }
-            tone(BUZZER, end_melody[thisNote], noteDuration * 0.9);
-            delay(noteDuration);
-            noTone(BUZZER);
-        }
-        //FND 출력
-        for(int i=0; i<=6; i++){
-            digitalWrite(i+3, FND[c_robot][i]);
-        }
-        for(int i=0; i<=6; i++){
-            digitalWrite(i+22, FND[c_human][i]);
-        }
-        for(int i=0; i<=6; i++){
-            if(now_led-past_led >= 5000000){
-                digitalWrite(i+3, 1);
-                digitalWrite(i+22, 1);
-                digitalWrite(13, 1);
-            }
-        }
-        //라인 LED 3번 깜빡이기
-        for(int i=0; i<3; i++){
-            digitalWrite(LINE_LED, 1);
-            delay(200);
-            digitalWrite(LINE_LED, 0);
-            delay(200);
-        }
-        digitalWrite(LINE_LED, 0);
-    } 
 }
